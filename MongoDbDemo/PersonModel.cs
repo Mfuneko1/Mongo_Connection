0eae256fe775389b483391a32e6e3ac636cfad10
Mfuneko April <mfunekoapril@gmail.com>
2022-01-11 18:13:32 +0200

    First commit to connecting to Mongo DB.

---
+++ MongoDbDemo/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbDemo
+{
+    public class PersonModel
+    {
+        public PersonModel()
+        {
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string  FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ MongoDbDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDbDemo;
+
+string connectionString = "mongodb://127.0.0.1:27017";
+string databaseName = "simple_db";
+string collectionName = "people";
+
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(databaseName);
+var collection = db.GetCollection<PersonModel>(collectionName);
+
+var person = new PersonModel { FirstName = "Mfuneko", LastName = "April" };
+
+//Inserting the values into the DB 
+await collection.InsertOneAsync(person);
+
+//Line to return every record from the DB where there's true
+var results = await collection.FindAsync(_ => true);
+
+foreach (var item in results.ToList())
+{
+    Console.WriteLine($"{item.Id}: {item.FirstName} {item.LastName}");
+}
